13b6c1fe6f6d70ab896268efc1f71660c9bf7ca2
G. Sun (4th Year - Woodland) <gs534@air208>
2018-11-06 13:11:04 +0000

    Shell scripts updated

--- runL2.sh
+++ runL2.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=1
-python trainL2.py --cuda --batchsize 2 --bptt 5 --naux 3 
+python trainL2.py --cuda --seed 1111 --nhid 256 --emsize 256 --lr 0.001 --batchsize 64 --bptt 30 --naux 64
